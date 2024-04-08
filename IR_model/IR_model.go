7c23c0a06ad4663f01c3f759938c59c2ab54299a
StephenJRead <read@usc.edu>
2024-04-07 19:35:11 -0700

    Updated 4/7/2024
    Updated to match changes in libraries

--- IR_model/IR_model.go
+++ IR_model/IR_model.go
@@ -400,7 +400,7 @@ func (ss *Sim) Init() {
 	ss.StopNow = false
 	ss.SetParams("", ss.LogSetParams) // all sheets
 	ss.NewRun()
-	ss.UpdateView(true)
+	ss.UpdateView(true, -1)
 }
 
 // InitRndSeed initializes the random seed based on current training run number
@@ -429,9 +429,9 @@ func (ss *Sim) Counters(train bool) string {
 	}
 }
 
-func (ss *Sim) UpdateView(train bool) {
+func (ss *Sim) UpdateView(train bool, cyc int) {
 	if ss.NetView != nil && ss.NetView.IsVisible() {
-		ss.NetView.Record(ss.Counters(train))
+		ss.NetView.Record(ss.Counters(train), cyc)
 		// note: essential to use Go version of update when called from another goroutine
 		ss.NetView.GoUpdate() // note: using counters is significantly slower..
 	}
@@ -473,11 +473,11 @@ func (ss *Sim) AlphaCyc(train bool) {
 				switch viewUpdt {
 				case leabra.Cycle:
 					if cyc != ss.Time.CycPerQtr-1 { // will be updated by quarter
-						ss.UpdateView(train)
+						ss.UpdateView(train, -1)
 					}
 				case leabra.FastSpike:
 					if (cyc+1)%10 == 0 {
-						ss.UpdateView(train)
+						ss.UpdateView(train, -1)
 					}
 				}
 			}
@@ -487,10 +487,10 @@ func (ss *Sim) AlphaCyc(train bool) {
 		if ss.ViewOn {
 			switch {
 			case viewUpdt <= leabra.Quarter:
-				ss.UpdateView(train)
+				ss.UpdateView(train, -1)
 			case viewUpdt == leabra.Phase:
 				if qtr >= 2 {
-					ss.UpdateView(train)
+					ss.UpdateView(train, -1)
 				}
 			}
 		}
@@ -498,9 +498,11 @@ func (ss *Sim) AlphaCyc(train bool) {
 
 	if train {
 		ss.Net.DWt()
+		ss.NetView.RecordSyns() // note: critical to update weights here so DWt is visible  
+		ss.Net.WtFmDWt()
 	}
 	if ss.ViewOn && viewUpdt == leabra.AlphaCycle {
-		ss.UpdateView(train)
+		ss.UpdateView(train, -1)
 	}
 	if ss.TstCycPlot != nil && !train {
 		ss.TstCycPlot.GoUpdate() // make sure up-to-date at end
@@ -589,7 +591,7 @@ func (ss *Sim) TrainTrial() {
 	if chg {
 		ss.LogTrnEpc(ss.TrnEpcLog)
 		if ss.ViewOn && ss.TrainUpdt > leabra.AlphaCycle {
-			ss.UpdateView(true)
+			ss.UpdateView(true, -1)
 		}
 		if ss.TestInterval > 0 && epc%ss.TestInterval == 0 { // note: epc is *next* so won't trigger first time
 			ss.TestAll()
@@ -751,7 +753,7 @@ func (ss *Sim) TestTrial(returnOnChg bool) {
 	_, _, chg := ss.TestEnv.Counter(env.Epoch)
 	if chg {
 		if ss.ViewOn && ss.TestUpdt > leabra.AlphaCycle {
-			ss.UpdateView(false)
+			ss.UpdateView(false, -1)
 		}
 		ss.LogTstEpc(ss.TstEpcLog)
 		if returnOnChg {
@@ -764,7 +766,7 @@ func (ss *Sim) TestTrial(returnOnChg bool) {
 	ss.TrialStats(false) // !accumulate
 	ss.LogTstTrl(ss.TstTrlLog)
 	if ss.NetData != nil { // offline record net data from testing, just final state
-		ss.NetData.Record(ss.Counters(false))
+		ss.NetData.Record(ss.Counters(false), -1, 1)
 	}
 }
 
@@ -1870,7 +1872,7 @@ func (ss *Sim) CmdArgs() {
 	}
 	if saveNetData {
 		ss.NetData = &netview.NetData{}
-		ss.NetData.Init(ss.Net, 200) // 200 = amount to save
+		ss.NetData.Init(ss.Net, 200, true) // 200 = amount to save
 	}
 	if ss.SaveWts {
 		fmt.Printf("Saving final weights per run\n")
